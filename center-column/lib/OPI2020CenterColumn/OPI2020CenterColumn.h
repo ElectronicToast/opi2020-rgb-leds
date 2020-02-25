fe7de5ba12d29d9c7fcebd698169b87d3ecd9b20
Ray Sun <rayjhsun@gmail.com>
2020-02-25 15:14:22 -0800

    Add files

---
+++ center-column/lib/OPI2020CenterColumn/OPI2020CenterColumn.h
@@ -0,0 +1,23 @@
+/**
+ * OPI2020CenterColumn.h
+ * Main header for OPI 2020 Center Column library
+ *
+ * Description:
+ *      This file is the main header file for the center column LED setup at 
+ *      OPI 2020, built using one Bifrost RGB LED controller
+ *
+ * Author:          Ray Sun
+ * Last Updated:    02/25/2020
+ */
+ 
+ 
+#ifndef OPI2020CC_H
+#define OPI2020CC_H
+
+
+#define     N_LEDS      60          // Number of LEDs per strip
+
+#define     N_STRIPS    6           // Number of strips in the center column
+
+
+#endif // OPI2020CC_H
